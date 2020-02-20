2d42c40270b9c633db0feb4d37cc81866904a359
Diego10Rocha <diego93rocha@gmail.com>
2020-02-20 01:46:16 +0000

    funções principaos 1 e 2

--- UserDAO.py
+++ UserDAO.py
@@ -1,43 +1,31 @@
 import json
 class UserDAO:
-	'''def getListUser():
-		users = []
-		try:
-			with open("usuarios.txt", "r") as arquivo:
-				if(arquivo):
-					arquivo.read()
-					for linha in arquivo:
-						linha = linha.strip()
-						usera.append(linha)
-		except:
-			print("O arquivo não existe!!")
 
-		return users'''
-	
 	def insertUser(self):
 		with open("usuarios.txt", "a") as arquivo:
-			arquivo.write('{"'+self.login+'":"'+self.password+'"}'+'\n')
-	
-	def getDicUser():
-		dicusers = {}
+			if(UserDAO.getDicUser("")):
+				arquivo.write(',"'+self.login+'":"'+self.password+'"')
+
+			else:
+				arquivo.write('"'+self.login+'":"'+self.password+'"')
+
+	def getDicUser(self):
 		try:
 			with open("usuarios.txt", "r") as arquivo:
 				userjson = arquivo.read()
+				userjson = '{'+userjson+'}'
 				dicusers = json.loads(userjson)
-				print(dicusers)
 				return dicusers
 		except:
-			print("")
-			
+			return ""
+
 	def login(nick, password):
 		users = {}
-		users = UserDAO.getDicUser()
-		print(users)
+		users = UserDAO.getDicUser("")
 		userverify = users.get(nick, False)
-		print(usersverify)
 		if(userverify == password):
 			print("Usuario logado!!")
 			return True
 		else:
-			print("Usuário e/ou senha incorreto(s) ")
+			print("Usuário e/ou senha incorreto(s)")
 			return False
--- main.py
+++ main.py
@@ -1,158 +1,81 @@
+from Task import Task
 from funcoes import *
 from User import User
 from UserDAO import UserDAO
 from TaskDAO import TaskDAO
-opcao=0
+option = 0
 
-while(opcao != 3):
-	opcao = PegarOpcaoDoMenu(3)
-	usersingularity = True
+while(option != 3):
+    option = PegarOpcaoDoMenu(3)
+    usersingularity = True
 
-	if(opcao == 1):
-		usersexists = False
-		userdao = UserDAO
-		users = userdao.getDicUser()
-		boolsingularity = False
-		if(users):
-			usersexists = True
-				
-		while(usersexists and usersingularity):
-			usersingularity = False
-			nickname = input("Digite seu nickname\n")
-			while(nickname==""):
-				nickname = input("Seu nickname não pode ser vazio\n")
-			if(users):
-				for elemento in users:
-					if(elemento == nickname):
-						usersingularity = True
-						boolsingularity = True
-						break
-			if(not usersingularity):
-				boolsingularity = False
-				break
+    if(option == 1):
+        usersexists = False
+        userdao = UserDAO
+        users = userdao.getDicUser("")
+        boolsingularity = False
+        if(users):
+            usersexists = True
 
-		if(not usersingularity):
-			nickname = input("Digite seu nickname\n")
+        while(usersexists and usersingularity):
+            usersingularity = False
+            boolsingularity = True
+            nickname = input("Digite seu nickname\n")
+            while(nickname==""):
+                nickname = input("Seu nickname não pode ser vazio, digite novamente\n")
+            if(users):
+                for elemento in users:
+                    if(elemento == nickname):
+                        usersingularity = True
+                        break
+            if(not usersingularity):
+                boolsingularity = False
+                break
 
-		password = input("Digite sua senha\n")
-		hashpass = CriptografarSenha(password)
+        if(not boolsingularity):
+            nickname = input("Digite seu nickname\n")
+            while(nickname==""):
+                nickname = input("Seu nickname não pode ser vazio, digite novamente\n")
 
-		user = User(nickname, hashpass)
-		userdao.insertUser(user)
+        password = input("Digite sua senha\n")
+        hashpass = CriptografarSenha(password)
 
-	elif(opcao == 2):
-		userdao = UserDAO
-		taskdao = TaskDAO
-		
-		nickname = input("Digite seu nickname\n")
-		password = input("Digite sua senha\n")
-		hashpass = CriptografarSenha(password)
+        user = User(nickname, hashpass)
+        userdao.insertUser(user)
 
-		boollog = userdao.login(nickname, hashpass)
-		if(boollog):
-			opcao = PegarOpcaoDoMenu(5)
-			while True:
-				opcao = PegarOpcaoDoMenu(5)
-				
-				if(opcao == 1):
-					titulo = input("Digite o titulo da tarefa")
-					descricao = input("Digite a descrição da tarefa")
-					prioridade = ""
-					while(prioridade!="1" and prioridade!="2" and prioridade!="3"):
-						prioridade = input("Digite a prioridade da tarefa:\n1- Alta \n2-Média \n3- Baixa")
+    elif(option == 2):
+        userdao = UserDAO
+        taskdao = TaskDAO
 
-					id_task = taskdao.getDicTask()
-					task = Task(id_task, titulo, descricao, prioridade)
-					taskdao.insertTask(task)
+        nickname = input("Digite seu nickname\n")
+        password = input("Digite sua senha\n")
+        hashpass = CriptografarSenha(password)
 
-				elif(opcao == 2):
-					print("opcao 2")
+        boollog = userdao.login(nickname, hashpass)
+        if(boollog):
+            while True:
+                option = PegarOpcaoDoMenu(5)
 
-				elif(opcao == 3):
-					print("opcao 3")
-				
-				elif(opcao ==4 ):
-					print("opcao 4")
+                if(option == 1):
+                    titulo = input("Digite o titulo da tarefa")
+                    descricao = input("Digite a descrição da tarefa")
+                    prioridade = ""
+                    while(prioridade!="1" and prioridade!="2" and prioridade!="3"):
+                        prioridade = input("Digite a prioridade da tarefa:\n1- Alta \n2-Média \n3- Baixa")
 
-				elif(opcao == 5):
-					opcao = PegarOpcaoDoMenu(3)
-					break
+                    id_task = taskdao.getDicTask()
+                    task = Task(id_task, titulo, descricao, prioridade)
+                    taskdao.insertTask(task)
 
+                elif(option == 2):
+                    print("opcao 2")
 
+                elif(option == 3):
+                    print("opcao 3")
 
+                elif(option == 4):
+                    print("opcao 4")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-#Cria o arquivo
-arquivo = open("usuarios.txt", "w")
-arquivo.write("Diego")
-arquivo.write("José")
-arquivo.close()
-
-#Ecreve no arquivo
-arquivo = open("usuarios.txt", "a")
-arquivo.write("\nDaniel\n")
-arquivo.write("Artur")
-arquivo.close()
-
-arquivo = open("usuarios.txt", "a")
-for i in range(10):
-	usuario = input("Digite seu nickname")
-	usuario = usuario + "\n"
-	arquivo.write(usuario)
-arquivo.close()'''
-'''
-#Lê o arquivo
-arquivo = open("usuarios.txt", "r")
-usuarios = []
-for linha in arquivo:
-	linha = linha.strip()
-	usuarios.append(linha)
-#print(arquivo.read())
-#linha = arquivo.readline()
-print(usuarios)
-arquivo.close()
-
-#O comando "with" fica responsável por fechar o arquivo após as operações terem sido terminadas
-with open('usuarios.txt') as arquivo:
-	for linha in arquivo:
-		print(linha)
-
-
-
-print("    _______________     ")
-print("   /               \ ")
-print("  /                 \ ")
-print("//                   \/\ ")
-print("\|      XXXX XXXX    | / ")
-print(" |      XXXX XXXX    |/ ")
-print(" |      XXX XXX      | ")
-print(" |                   | ")
-print(" \__      XXX      __/ ")
-print("   |\     XXX     /| ")
-print("   | |           | | ")
-print("   | I I I I I I I | ")
-print("   |  I I I I I I  | ")
-print("    \_           _/ ")
-print(" 	 \_     	_/ ")
-print(" 	   \_______/ ")
-'''
+                elif(option == 5):
+                    option = PegarOpcaoDoMenu(3)
+                    break
